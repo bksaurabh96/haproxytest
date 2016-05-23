cc34dfff10f2c0a5088c207ad0e5051c2541b82e
saurabh <saurabh.goyal@reancloud.com>
2016-05-23 15:56:00 +0530

    adding thet wrapper

---
+++ chef-repo/cookbooks/cicada-haproxy/metadata.rb
@@ -0,0 +1,9 @@
+name             'cicada-haproxy'
+maintainer       'ReanCloud'
+maintainer_email 'saurabh.goyal@reancloud.com'
+license          'All rights reserved'
+description      'Installs/Configures haproxy'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends 'haproxy'
---
+++ chef-repo/cookbooks/cicada-haproxy/recipes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: haproxy
+# Recipe:: default
+#
+# Copyright 2016, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe 'haproxy'
