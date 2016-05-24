3ebf3d0f5c67579a1ab4f9443128b7e933358d85
saurabh <saurabh.goyal@reancloud.com>
2016-05-25 02:00:47 +0530

    New changes

--- chef-repo/cookbooks/cicada-haproxy/recipes/default.rb
+++ chef-repo/cookbooks/cicada-haproxy/recipes/default.rb
@@ -8,3 +8,18 @@
 #
 
 include_recipe 'haproxy'
+include_recipe 'rsyslog'
+
+#Enable haproxy logs file
+cookbook_file "/etc/rsyslog.d/haproxy.conf" do
+  source "haproxy.conf"
+  owner "root"
+  group "root"
+  mode 00644
+end
+
+#Restart rsyslog service to enable logging
+service node['rsyslog']['service_name'] do
+  supports restart: true, status: true
+  action [:enable, :start]
+end
